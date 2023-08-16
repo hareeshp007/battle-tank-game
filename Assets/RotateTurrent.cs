8ffa2f6b9388f3e2d314c3493baadf9bff95c904
Hareesh P <46471092+hareeshp007@users.noreply.github.com>
2023-08-16 11:33:17 +0530

    Rotation of Torrent And Tank

--- Assets/RotateTurrent.cs
+++ Assets/RotateTurrent.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class RotateTurrent : MonoBehaviour
 {
+    public Joystick joystick;
+    public float rotSpeed = 3f;
+    [SerializeField]
+    private float rotationSpeed=2f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,13 @@ public class RotateTurrent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RotationTurrent();
+    }
+    private void RotationTurrent()
+    {
+        Vector3 move = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
+        Quaternion targetRotation = Quaternion.LookRotation(move, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+
     }
 }
--- Assets/Tank.cs
+++ Assets/Tank.cs
@@ -9,8 +9,9 @@ public class Tank : MonoBehaviour
     public CharacterController characterController;
 
     public float speed=40f;
-    float HorizontalMove = 0f;
-    float VerticalMove = 0f;
+    [SerializeField]
+    private float rotationSpeed=3f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,24 +26,11 @@ public class Tank : MonoBehaviour
     }
     private void MoveTank()
     {
-        if (joystick.Horizontal >= .2f)
-        {
-            HorizontalMove = speed;
-        }
-        else if (joystick.Horizontal <= -.2f)
-        {
-            HorizontalMove = -speed;
-        }
-        else if (joystick.Vertical >= .2f)
-        {
-            VerticalMove = speed;
-        }
-        else if (joystick.Vertical <= -.2f)
-        {
-            VerticalMove = -speed;
-        }
-        Debug.Log(HorizontalMove + " " + VerticalMove);
         Vector3 move = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
         characterController.Move(move * Time.deltaTime *speed);
+        Quaternion targetRotation = Quaternion.LookRotation(move, Vector3.up);
+
+        // Smoothly interpolate the rotation using Lerp
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
     }
 }
